05907bbaef020ce4486c94ea6bfe55116dca8975
Azrand <kregerdennis@gmail.com>
2022-08-30 09:39:21 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from profile import run
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import app
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect = True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API! 
+    <br />
+    <br />
+    Available Routes:<br />
+    /api/v1.0/precipitation <br />
+    /api/v1.0/stations <br />
+    /api/v1.0/tobs <br />
+    /api/v1.0/temp/start/end <br />
+    ''')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
